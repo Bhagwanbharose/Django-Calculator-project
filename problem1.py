bb8ae984fd6d1774aa2139df587a15ae297a3b48
santoshgopane <santoshgopane>
2020-03-15 01:53:00 +0530

    Python commit

---
+++ problem1.py
@@ -0,0 +1,12 @@
+name = input('Enter String :')
+k = input('check frequncy of :')
+
+def split1(name):
+	return [c for c in name]
+
+new = split1(name)
+n = 0
+for i in new:
+	if i == k:
+		n = n + 1
+print('The Frequncy of {} is :{}'.format(k,n))
